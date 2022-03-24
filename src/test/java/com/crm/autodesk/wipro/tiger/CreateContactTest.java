38a235fc58a9d5af53bbfb733cc618d91e2e98e7
soumya lall <soumya lall@LAPTOP-CU2M7T7J>
2022-03-24 14:59:58 +0530

    user1 first commit

---
+++ src/test/java/com/crm/autodesk/wipro/tiger/CreateContactTest.java
@@ -0,0 +1,11 @@
+package com.crm.autodesk.wipro.tiger;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void createcontct()
+	{
+System.out.println("contact is created");
+}
+}
